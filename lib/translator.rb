e96e560169dca015bf71fbc8be0cdce224e7aa2d
Aaron Chu <aaronchu357@gmail.com>
2019-04-11 00:15:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require "pry"
 
 def load_library(file)
   emoticons = YAML.load_file(file)
-  hash = {}
+  hash = Hash.new
   hash["get_emoticon"] = {}
   hash["get_meaning"] = {}
 
@@ -11,7 +11,7 @@ def load_library(file)
     hash["get_emoticon"][set.first] = set.last
     hash["get_meaning"][set.last] = word
   end
-  return hash
+  hash
 end
 
 def get_japanese_emoticon
